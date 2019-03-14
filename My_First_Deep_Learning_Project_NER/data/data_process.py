f3d237c90edd734f292a767fd2c51d2eea197392
ArtificialIdoit <42697905+ArtificialIdoit@users.noreply.github.com>
2019-03-14 20:28:55 +0800

    Add files via upload

--- My_First_Deep_Learning_Project_NER/data/data_process.py
+++ My_First_Deep_Learning_Project_NER/data/data_process.py
@@ -3,7 +3,8 @@
 import codecs
 import re
 import pdb
-import pandans as pd
+import numpy as np
+import pandas as pd
 import collections
 from compiler.ast import flatten
 
@@ -100,9 +101,12 @@ def data2pkl():
             tags.add(word[1])
             if word[1] != 'O':
                 numNotO+=1
+
         if numNotO!=0:
+            # 猜测是因为数据不对称，所以要删除一部分无用行
             datas.append(linedata)
             labels.append(linelabel)
+
     input_data.close()
     print(len(datas))
     print(len(labels))
@@ -115,6 +119,52 @@ def data2pkl():
     set_words = sr_allwords.index
     set_ids = range(1,len(set_words)+1)
 
+    tags = [i for i in tags]
+    tag_ids = range(len(tags))
+    word2id = pd.Series(set_ids,index=set_words)
+    id2word = pd.Series(set_words,index=set_ids)
+    tag2id = pd.Series(tag_ids, index=tags)
+    id2tag = pd.Series(tags, index=tag_ids)
+    word2id['unknown'] = len(word2id)+1
+    id2word[len(id2word)] = 'unknown'
+    print(tag2id)
+    max_len = 60
+
+    def x_padding(words):
+        ids = list(word2id[words])
+        if len(ids) >= max_len:
+            return ids[:max_len]
+        ids.extend([0]*(max_len-len(ids)))
+        return ids
+
+    def y_padding(tags):
+        ids = list(tag2id[tags])
+        if len(ids) >= max_len:
+            return ids[:max_len]
+        ids.extend([0] * (max_len - len(ids)))
+        return ids
+    df_data = pd.DataFrame({'words':datas,'tags':labels},index=range(len(datas)))
+    df_data['x'] = df_data['words'].apply(x_padding)
+    df_data['y'] = df_data['tags'].apply(y_padding)
+    x = np.asarray(list(df_data['x'].values))
+    y = np.asarray(list(df_data['y'].values))
+
+    from sklearn.model_selection import train_test_split
+    x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=43)
+    x_train,x_valid,y_train,y_valid = train_test_split(x_train,y_train,test_size=0.2,random_state=43)
+
+    import  pickle
+    with codecs.open('../renmindata.pkl','wb') as outp:
+        pickle.dump(word2id, outp)
+        pickle.dump(id2word, outp)
+        pickle.dump(tag2id, outp)
+        pickle.dump(id2tag, outp)
+        pickle.dump(x_train, outp)
+        pickle.dump(y_train, outp)
+        pickle.dump(x_test, outp)
+        pickle.dump(y_test, outp)
+        pickle.dump(x_valid, outp)
+        pickle.dump(y_valid, outp)
 
 
 
